25bf1352b6de26447c80e1c8124b7c111dc7454a
LiadNahum1 <liadna@post.bgu.ac.il>
2018-04-03 00:25:03 +0300

    LogicLayer interfaces

---
+++ LogicLayer/IChatRoom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatRoomProject.CommunicationLayer;
+
+namespace ChatRoomProject.LogicLayer
+{
+    public interface IChatRoom
+    {
+        void Start();
+        bool Registration(string groupId, string nickname);
+        bool IsValidRegistration(string groupId, string nickname);
+        bool IsValidLogin(string groupId, string nickname);
+        bool Login(string groupId, string nickname);
+        void Logout();
+        void RetrieveNMessages(int number);
+        List<IMessage> DisplayNMessages(int number);
+        void Send(string messageContent);
+
+    }
+}
---
+++ LogicLayer/IUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatRoomProject.CommunicationLayer;
+
+namespace ChatRoomProject.LogicLayer
+{
+    public interface IUser
+    {
+        string GroupID();
+        string Nickname();
+        IMessage Send(string messageContent);
+
+    }
+}

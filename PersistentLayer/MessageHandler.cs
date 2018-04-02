d380fcf0d11471158c8d85480cc22e0bfbea125e
abensour <37442472+abensour@users.noreply.github.com>
2018-04-03 01:04:56 +0300

    presistent Layer almost ready

---
+++ PersistentLayer/MessageHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ChatRoomProject.PersistentLayer
+{
+    class MessageHandler
+    {
+        public static void SaveToFile(Guid id, string nickname, string groupID, DateTime date, string messageContent)
+        {
+            string startupPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            startupPath += "\\PersistentLayer\\DataMessages.txt";
+            using (StreamWriter sw = File.AppendText(startupPath))
+            {
+                sw.WriteLine(id.ToString() + "," + nickname + "," + groupID + "," + date.ToString() + "," + messageContent );
+              
+            }
+        }
+        public static List<String> RestoreMessages()
+        {
+            List<string> messageList = new List<string>();
+            string startupPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            startupPath += "\\PersistentLayer\\DataMessages.txt";
+            var lines = System.IO.File.ReadAllLines(startupPath);
+            foreach (string item in lines)
+            {
+                messageList.Add(item);
+            }
+            return messageList;
+
+        }
+    }
+}

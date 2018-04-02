d1633f00b01964ef271924f82245a31cd84b32d4
NoaFeiger <noafeiger29@gmail.com>
2018-04-03 01:35:22 +0300

    Main in the PresentationLayer

---
+++ PresentationLayer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatRoomProject.LogicLayer;
+using ChatRoomProject.PresentationLayer;
+using System.IO;
+using log4net;
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+namespace ChatRoomProject
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger("Program.cs");
+        static void Main(string[] args)
+        {
+           
+                ChatRoom cr = new ChatRoom();
+                cr.Start();
+                Gui gui = new Gui(cr);
+                gui.Start();
+
+
+        }
+    }
+}
